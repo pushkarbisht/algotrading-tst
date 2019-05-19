7d50b24783075be75810639dfcb091348fccfb39
Pushkar Bisht <Pushkar.Bisht@veritas.com>
2019-05-19 16:43:32 +0530

    latest

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.get('/start', function (req, res) {
 
 
 
-var server = app.listen(8081, function () {
+var server = app.listen(80, function () {
    var host = server.address().address
    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Example app listening at http://%s:%s", "0.0.0.0", port)
 })
