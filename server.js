4543601233558ddc56803587306f5b16c246f906
Pushkar Bisht <Pushkar.Bisht@veritas.com>
2019-05-19 16:27:11 +0530

    initial

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+
+app.use(express.static('public')); 
+
+app.get('/', function (req, res) {
+    res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.get('/start', function (req, res) {
+    res.send('Page Pattern Match');
+})
+
+
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
